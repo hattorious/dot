6812c6f9615de1d47532a610b5669553aa869db5
Ross Hattori <git@exstasis.net>
2015-09-12 15:12:26 -0700

    Update install_fonts

---
+++ fonts/install_fonts.bash
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -euo pipefail
+IFS=$'\n\t'
+. "$DOTFILES/bash/init.bash"
+
+# OSX only, abort otherwise.
+is_osx || return 1
+
+rm -rf ~/Library/Fonts/dotbot
+mkdir -p ~/Library/Fonts/dotbot
+cp -a "$DOTFILES/fonts/mplus-TESTFLIGHT-060" ~/Library/Fonts/dotbot/mplus
