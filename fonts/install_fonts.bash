12f4b9a3b0b103cfad0fb8a8710a27f8b75a80ed
Ross Hattori <git@exstasis.net>
2016-03-02 12:19:22 -0800

    Add Anonymous Pro to the installed fonts

--- fonts/install_fonts.bash
+++ fonts/install_fonts.bash
@@ -9,3 +9,4 @@ is_osx || return 1
 rm -rf ~/Library/Fonts/dotbot
 mkdir -p ~/Library/Fonts/dotbot
 cp -a "$DOTFILES/fonts/mplus-TESTFLIGHT-060" ~/Library/Fonts/dotbot/mplus
+cp -a "$DOTFILES/fonts/anonymice-powerline" ~/Library/Fonts/dotbot/anonymice
