2465812483ccda0baf2ebf182564a009a0b2952f
Ross Hattori <git@exstasis.net>
2016-04-20 21:03:13 -0700

    feat: Add bash completions for nodenv to freebsd

--- bash/runcom.freebsd.bash
+++ bash/runcom.freebsd.bash
@@ -6,3 +6,4 @@ is_freebsd || return 1
 ## NODENV
 export PATH=$PATH:$HOME/nodenv/bin:
 [[ "$(which nodenv)" ]] && export NODENV_ROOT=$HOME/nodenv && eval "$(nodenv init -)"
+[[ -f $NODENV_ROOT/completions/nodenv.bash ]] && . "$NODENV_ROOT"/completions/nodenv.bash
