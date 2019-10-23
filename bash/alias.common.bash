bbe15e5469b3de7337b091a008d3758198ba35eb
Ross Hattori <git@hattori.me>
2019-10-23 11:07:02 +0100

    feat: Cleanup and expand some standard bash functions

--- bash/alias.common.bash
+++ bash/alias.common.bash
@@ -5,9 +5,8 @@ alias cd..='cd ..'
 alias c='clear'
 
 
-# An overly obvious reference for most commonly requested bash
-# timestamps
-function dates() {
+# An overly obvious reference for most commonly requested bash timestamps
+function ref_dates() {
 cat << EOD
         Format/result         |       Command              |          Output
 ------------------------------+----------------------------+------------------------------
@@ -25,6 +24,67 @@ ISO8601 Local TZ timestamp    | date +%FT%T%Z              | $(date +%FT%T%Z)
 EOD
 }
 
+
+# http://stackoverflow.com/questions/3869072/test-for-non-zero-length-string-in-bash-n-var-or-var
+# designed to fit an 80 character terminal
+# by Dennis Williamson
+# shellcheck disable=SC2015,SC2070,SC2086
+ref_null_unset() (
+    dw=5    # description column width
+    w=6     # table column width
+
+    t () { printf "%-${w}s" "true"; }
+    f () { [[ $? == 1 ]] && printf "%-${w}s" "false " || printf "%-${w}s" "-err- "; }
+
+    o=/dev/null
+
+    echo '       1a    2a    3a    4a    5a    6a    |1b    2b    3b    4b    5b    6b'
+    echo '       [     ["    [-n   [-n"  [-z   [-z"  |[[    [["   [[-n  [[-n" [[-z  [[-z"'
+
+    while read -r d t; do
+        printf "%-${dw}s: " "$d"
+
+        case $d in
+            unset) unset t  ;;
+            space) t=' '    ;;
+        esac
+
+        [ $t ]        2>$o  && t || f
+        [ "$t" ]            && t || f
+        [ -n $t ]     2>$o  && t || f
+        [ -n "$t" ]         && t || f
+        [ -z $t ]     2>$o  && t || f
+        [ -z "$t" ]         && t || f
+        echo -n "|"
+        [[ $t ]]            && t || f
+        [[ "$t" ]]          && t || f
+        [[ -n $t ]]         && t || f
+        [[ -n "$t" ]]       && t || f
+        [[ -z $t ]]         && t || f
+        [[ -z "$t" ]]       && t || f
+        echo
+    done <<'EOF'
+unset
+null
+space
+zero    0
+digit   1
+char    c
+hyphn   -z
+two     a b
+part    a -a
+Tstr    -n a
+Fsym    -h .
+T=      1 = 1
+F=      1 = 2
+T!=     1 != 2
+F!=     1 != 1
+Teq     1 -eq 1
+Feq     1 -eq 2
+Tne     1 -ne 2
+Fne     1 -ne 1
+EOF
+)
 
 ## npm package maintenance
 alias patch='pre-version && npm version patch && post-version'
@@ -56,6 +116,12 @@ function read_file() {
     if [[ -f $1 ]]; then
         while IFS= read -r line || [ -n "$line" ]; do echo "$line"; done <"$1"
     fi
+    unset IFS
 }
 
-alias killgpg='gpgconf --kill gpg-agent'
+function killgpg() {
+  if [[ "$(command -v gpg-agent)" ]]; then
+      gpgconf --kill gpg-agent;
+      gpg-connect-agent /bye;
+  fi
+}
