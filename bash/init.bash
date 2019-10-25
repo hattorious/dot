e1047d30656b3442c7af32918c16caf73d5102a1
Ross Hattori <git@hattori.me>
2019-10-25 08:37:02 +0100

    feat: Update bash OS detection

--- bash/init.bash
+++ bash/init.bash
@@ -4,13 +4,41 @@
 # Let's get started.
 export DOTFILES=~/.dotfiles
 
+function is_interactive_shell() {
+  # are we running in an interactive shell?
+  [[ $- == *i* ]] || return 1
+}
+
 # OS detection
 function is_osx() {
     [[ "$OSTYPE" =~ ^darwin ]] || return 1
 }
-function is_ubuntu() {
-    [[ "$(cat /etc/issue 2> /dev/null)" =~ Ubuntu ]] || return 1
+
+# read the `os-release` file and echo back the passed variable
+# https://www.freedesktop.org/software/systemd/man/os-release.html
+function read_os() {
+  local echovar=${1:-PRETTY_NAME}
+  local osrelease
+  # first find which file to source
+  if [[ -f /etc/os-release ]]; then
+    osrelease="/etc/os-release"
+  else
+    osrelease="/usr/lib/os-release"
+  fi
+
+  # now source the file in a subshell to prevent polluting the shell variables of the 
+  # current shell, and bubble up the variable we're interested in
+  echo "$(
+    . $osrelease;
+    # we don't trust that the sourced file didn't change the PATH
+    $(command -v echo) "${!echovar}";
+  )"
 }
+
+function is_ubuntu() {
+  [[ "$(read_os ID)" == "ubuntu" ]] || return 1
+}
+
 function is_freebsd() {
     [[ "$OSTYPE" =~ ^freebsd ]] || return 1
 }
